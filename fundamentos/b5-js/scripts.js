14800d8754d314b8cec292d808d44fe28c21e81d
unknown <wesleyrenamjob@gmail.com>
2022-08-19 14:56:45 -0300

    Exercicios de JavaSc adicionados

---
+++ fundamentos/b5-js/scripts.js
@@ -0,0 +1,8 @@
+let age = 15;
+console.log(age);
+const myName = "Wesley";
+const birthCity = "Aquidabã";
+let birthYear = "2002";
+console.log(myName, birthCity, birthYear)
+birthYear = 30;
+console.log(birthYear)
